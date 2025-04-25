d3fb0ca6e7bdd4d2d8c7fc33376156d5efeaab1f
agonzc23 <agonzc23@estudiantes.unileon.es>
2025-04-25 10:47:22 +0200

    Dockerized courses microservice, databases, and communication between msvc

--- msvc-cursos/src/main/java/org/agonzc/springcloud/msvc/cursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/org/agonzc/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "msvc-usuarios", url = "localhost:8081")
+@FeignClient(name = "msvc-usuarios", url = "msvc-usuarios:8081")
 public interface UsuarioClientRest {
 
 	@GetMapping("/{id}")
